93828bd4c4de549b3a2a51ff534cb56b55f1e7b1
Júlia Otranto Aulicino <julia.otranto@outlook.com>
2026-02-25 22:15:06 -0300

    Lets see

---
+++ src/plugin-main.m
@@ -0,0 +1,392 @@
+/*
+OBS Projector
+Copyright (C) 2025 Julia Otranto Aulicino
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program. If not, see <https://www.gnu.org/licenses/>
+*/
+
+// TODO: Adjust for Windows Platform
+#define CONFIG_FILE "~/projection-config.json"
+
+#import <Cocoa/Cocoa.h>
+
+#include <obs-module.h>
+#include <plugin-support.h>
+#include <stdlib.h>
+
+#include <uv.h>
+#include <tinycthread.h>
+
+#include "debug.h"
+#include "config-structs.h"
+#include "config.h"
+#include "config-debug.h"
+#include "monitor.h"
+#include "loop.h"
+#include "render.h"
+#include "render-obs.h"
+#include "ogl-loader.h"
+
+static uv_loop_t *loop;
+
+static int initialized = 0;
+static int configured = 0;
+
+static int last_width = 0;
+static int last_height = 0;
+
+static projection_config *config;
+static obs_output_t *output;
+
+typedef struct {
+    obs_output_t *output;
+} context_info;
+
+void glfwIntErrorCallback(GLint _, const GLchar *error_string) {
+    log_debug("Catch GLFW error: %s\n", error_string);
+}
+
+void glfwIntMonitorCallback(GLFWmonitor* monitor, int event) {
+    if (config) {
+        monitors_reload();
+        monitors_adjust_windows(config);
+    }
+}
+
+static thrd_t pool_thread_id;
+static int pool_running;
+static int pool_thread_running;
+
+void pool_ui_events() {
+    if (pool_thread_running == 0) {
+        return;
+    }
+    
+    glfwPollEvents();
+    
+    if (window_should_close()) {
+        log_debug("window should close is true\n");
+        obs_output_stop(output);
+        obs_output_release(output);
+        output = NULL;
+    }
+
+    pool_running = 0;
+}
+
+int pool_loop(void *_) {
+    struct timespec sleep_interval;
+
+    while (pool_thread_running) {
+        sleep_interval.tv_nsec = 0;
+        sleep_interval.tv_sec = 1;
+
+        thrd_sleep(&sleep_interval, 0);
+
+        if (pool_running == 0 && pool_thread_running) {
+            pool_running = 1;
+
+            obs_queue_task(OBS_TASK_UI, pool_ui_events, 0, false);
+        }
+    }
+
+    return 0;
+}
+
+void internal_lib_render_shutdown() {
+    log_debug("Shutting down main loop...");
+    main_loop_terminate();
+
+    log_debug("Shutting down renders...");
+    shutdown_renders();
+
+    log_debug("Shutting down monitors...");
+    monitors_destroy_windows();
+}
+
+void internal_lib_render_load_default_config() {
+    config_bounds default_monitor;
+
+    monitors_get_default_projection_bounds(&default_monitor);
+    prepare_default_config(&default_monitor);
+}
+
+void internal_lib_render_startup() {
+    log_debug("Will load config:");
+    print_projection_config(config);
+
+    log_debug("Initialize renders...");
+    initialize_renders();
+
+    log_debug("Creating windows...");
+    monitors_create_windows(config);
+
+    log_debug("Initializing async transfer windows...");
+    activate_renders(monitors_get_shared_window());
+
+    log_debug("Starting main loop...")
+    main_loop_schedule_config_reload(config);
+    main_loop_start();
+}
+
+void internal_lib_render_load_config() {
+    configured = 0;
+
+    log_debug("Reloading monitors");
+    monitors_reload();
+
+    log_debug("Creating default config");
+    internal_lib_render_load_default_config();
+
+    projection_config *new_config;
+
+    log_debug("Loading screen configs");
+    new_config = load_config(CONFIG_FILE);
+
+    if (new_config == NULL) {
+        generate_config(CONFIG_FILE);
+        new_config = load_config(CONFIG_FILE);
+    }
+
+    if (new_config == NULL) {
+        new_config = config_get_default();
+    }
+
+    if (config) {
+        if (config_change_requires_restart(new_config, config)) {
+            internal_lib_render_shutdown();   
+            free_projection_config(config);
+        } else {
+            log_debug("New config was loaded! hot reloading...");
+            projection_config* old_config = config;
+
+            config = new_config;
+
+            main_loop_schedule_config_reload(config);
+            free_projection_config(old_config);
+
+            return;
+        }
+    }
+
+    log_debug("Staring engine...");
+
+    config = new_config;
+
+    internal_lib_render_startup();
+
+    configured = 1;
+}
+
+const char* my_output_name(void* type_data) {
+	return "Projector Output";
+}
+
+void* my_output_create(obs_data_t *settings, obs_output_t *output) {
+    config = NULL;
+
+    obs_log(LOG_INFO, "Output will be created");
+
+    if (glfwInit()) {
+        initialized = 1;
+
+        glfwSetErrorCallback(glfwIntErrorCallback);
+        glfwSetMonitorCallback(glfwIntMonitorCallback);
+
+        pool_thread_running = 1;
+        thrd_create(&pool_thread_id, pool_loop, NULL);
+
+        obs_log(LOG_INFO, "glfw initialized");
+    } else {
+        obs_log(LOG_INFO, "Failed to initialize glfw");
+        return (void*)NULL;
+    }
+
+    context_info *info = bzalloc(sizeof(context_info));
+    info->output = output;
+
+    obs_log(LOG_INFO, "Output created");
+
+	return (void*)info;
+
+}
+
+void my_output_update(void *data, obs_data_t *settings) {
+
+}
+
+void my_output_destroy(void *data) {
+    log_debug("output will destroy");
+    
+    if (configured) {
+        internal_lib_render_shutdown();
+
+        log_debug("lib render shutdown finish");
+        configured = 0;
+        config = NULL;
+    }
+
+    if (initialized) {
+        initialized = 0;
+        pool_thread_running = 0;
+        log_debug("terminating glfw");
+        glfwTerminate();
+        obs_log(LOG_INFO, "glfw terminated");
+    } else {
+        obs_log(LOG_INFO, "Try to destroy not created output");
+    }
+
+    log_debug("output destroyed");
+}
+
+bool my_output_start(void *data) {
+    if (!initialized) {
+        return false;
+    }
+
+    context_info *info = (context_info*) data;
+
+    internal_lib_render_load_config();
+
+    obs_output_begin_data_capture(info->output, 0);
+    return true;
+}
+
+void my_output_stop(void *data, uint64_t ts) {
+    obs_log(LOG_INFO, "output will stop");
+
+    if (!initialized) {
+        return;
+    }
+
+    context_info *info = (context_info*) data;
+
+    if (configured) {
+        configured = 0;
+        
+        obs_output_end_data_capture(info->output);
+
+        internal_lib_render_shutdown();
+        last_width = 0;
+        last_height = 0;
+        config = NULL;
+    }
+
+    log_debug("output stopped");
+}
+
+void my_output_data(void *data, struct video_data *frame) {
+    if (!configured) {
+        return;
+    }
+
+    context_info *info = (context_info*) data;
+
+    int width = obs_output_get_width(info->output);
+    int height = obs_output_get_height(info->output);
+
+    uint8_t *video_data = frame->data[0];
+
+    if (width && height && data && frame->linesize[0]) {
+        renders_push_frame(video_data, width, frame->linesize[0], height);
+        if (width != last_width || height != last_height) {
+            last_width = width;
+            last_height = height;
+            main_loop_schedule_config_reload(config);
+        }
+    }
+}
+
+void handle_uv_fs_event(uv_fs_event_t *handle, const char *filename, int events, int status) {
+    // internal_lib_render_load_config();
+}
+
+struct obs_output_info my_output = {
+        .id                   = "projector",
+        .flags                = OBS_OUTPUT_VIDEO,
+        .get_name             = my_output_name,
+        .create               = my_output_create,
+		.update               = my_output_update,
+        .destroy              = my_output_destroy,
+        .start                = my_output_start,
+        .stop                 = my_output_stop,
+        .raw_video            = my_output_data
+};
+
+OBS_DECLARE_MODULE()
+OBS_MODULE_USE_DEFAULT_LOCALE(PLUGIN_NAME, "en-US")
+
+bool obs_module_load(void)
+{
+    loop = uv_default_loop();
+    uv_run(loop, UV_RUN_DEFAULT);
+
+    [NSApplication sharedApplication];
+    NSApplicationDelegate oldDelegate = [NSApp delegate];
+
+    glfwInit();
+
+    // obs_register_output(&my_output);
+
+    // output = obs_output_create("projector", "Projector Output", NULL, NULL);
+    // obs_output_set_media(output, obs_get_video(), NULL);
+
+    int width = obs_output_get_width(output);
+    int height = obs_output_get_height(output);
+
+    const struct video_scale_info scale_info = {
+        .format = VIDEO_FORMAT_BGRA,
+        .width = width,
+        .height = height,
+        .range = VIDEO_RANGE_FULL,
+        .colorspace = VIDEO_CS_SRGB,
+    };
+    
+    // obs_output_set_video_conversion(output, &scale_info);
+    bool success = 1; // obs_output_start(output);
+
+    obs_log(LOG_INFO, "plugin started successfully");
+
+    uv_fs_event_t *fs_event_req = malloc(sizeof(uv_fs_event_t));
+    uv_fs_event_init(loop, fs_event_req);
+    uv_fs_event_start(fs_event_req, handle_uv_fs_event, CONFIG_FILE, 0);
+
+    glfwTerminate();
+
+    [NSApp setDelegate:oldDelegate];
+
+    if (!success) {
+        const char *error = obs_output_get_last_error(output);
+        obs_log(LOG_ERROR, "Failed to start output %s", error);
+    } else {
+        obs_log(LOG_INFO, "plugin loaded successfully (version %s)", PLUGIN_VERSION);
+    }
+
+	return success;
+}
+
+void obs_module_unload(void)
+{
+    obs_log(LOG_INFO, "plugin will unload");
+
+    // if (output) {
+    //     obs_output_stop(output);
+    //     obs_output_release(output);
+    //     output = NULL;
+    // }
+
+    uv_loop_close(loop);
+
+	obs_log(LOG_INFO, "plugin unloaded");
+}
